ed5ae33c5e76a58a66abfacf7d8372405520bcd0
Michael Lazebny <michael@lazebny.io>
2024-01-05 15:39:03 +0100

    feat: Implemented sign out

--- user_service/internal/user/api/sign_out.go
+++ user_service/internal/user/api/sign_out.go
@@ -7,5 +7,16 @@ import (
 )
 
 func (h *userHandlerImpl) SignOut(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+	refreshToken, err := getRefreshTokenFromQueryParams(r)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	if err = h.service.SignOut(r.Context(), refreshToken); err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusOK)
 	return nil
 }

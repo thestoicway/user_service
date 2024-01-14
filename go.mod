fb36136ae5a50de6fc968dbcba33793dc8e4c3f3
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2024-01-14 13:24:26 +0100

    test: added tests for sign up service method (#18)
    * test: added tests for sign up service method
    
    * removed wrong tests

--- go.mod
+++ go.mod
@@ -11,7 +11,9 @@ require (
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/redis/go-redis/v9 v9.4.0
+	github.com/stretchr/testify v1.8.4
 	github.com/thestoicway/custom_errors v0.0.2
+	go.uber.org/mock v0.4.0
 	go.uber.org/zap v1.26.0
 	golang.org/x/crypto v0.18.0
 	gorm.io/driver/postgres v1.5.4
@@ -22,6 +24,7 @@ require (
 	ariga.io/atlas-go-sdk v0.3.0 // indirect
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -37,7 +40,7 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.19 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	go.uber.org/mock v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
---
+++ internal/usr/service/sign_up_test.go
@@ -0,0 +1,85 @@
+package service_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/thestoicway/user_service/internal/usr/jsonwebtoken"
+	mocks "github.com/thestoicway/user_service/internal/usr/mocks"
+	"github.com/thestoicway/user_service/internal/usr/model"
+	"github.com/thestoicway/user_service/internal/usr/service"
+	"go.uber.org/mock/gomock"
+)
+
+func TestSignUp(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDatabase := mocks.NewMockUserDatabase(ctrl)
+	mockJwtManager := mocks.NewMockJwtManager(ctrl)
+	mockSession := mocks.NewMockSessionDatabase(ctrl)
+
+	s := service.NewUserService(&service.UserServiceParams{
+		Database:   mockDatabase,
+		JwtManager: mockJwtManager,
+		Session:    mockSession,
+	})
+
+	user := &model.User{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	ctx := context.Background()
+
+	t.Run("successful sign up", func(t *testing.T) {
+		mockDatabase.EXPECT().InsertUser(gomock.Any(), gomock.Any()).Return(&uuid.UUID{}, nil)
+		mockJwtManager.EXPECT().GenerateTokenPair(gomock.Any()).Return(&jsonwebtoken.TokenPair{}, &jsonwebtoken.AdditionalInfo{}, nil)
+		mockSession.EXPECT().AddSession(gomock.Any(), gomock.Any()).Return(nil)
+
+		_, err := s.SignUp(ctx, user)
+		assert.NoError(t, err)
+	})
+
+	t.Run("error when generating password hash", func(t *testing.T) {
+		user.Password = string(make([]byte, 1001)) // bcrypt can handle up to 72 bytes
+
+		_, err := s.SignUp(ctx, user)
+		assert.Error(t, err)
+	})
+
+	t.Run("error when inserting user into the database", func(t *testing.T) {
+		user.Password = "password"
+		mockDatabase.EXPECT().InsertUser(gomock.Any(), gomock.Any()).Return(&uuid.UUID{}, errors.New("database error"))
+
+		_, err := s.SignUp(ctx, user)
+		assert.Error(t, err)
+	})
+
+	t.Run("error when generating token pair", func(t *testing.T) {
+		mockDatabase.EXPECT().InsertUser(gomock.Any(), gomock.Any()).Return(&uuid.UUID{}, nil)
+		mockJwtManager.EXPECT().GenerateTokenPair(gomock.Any()).Return(nil, nil, errors.New("token error"))
+
+		_, err := s.SignUp(ctx, user)
+		assert.Error(t, err)
+	})
+
+	t.Run("error when adding session", func(t *testing.T) {
+		mockDatabase.EXPECT().InsertUser(gomock.Any(), gomock.Any()).Return(&uuid.UUID{}, nil)
+		mockJwtManager.EXPECT().GenerateTokenPair(gomock.Any()).Return(&jsonwebtoken.TokenPair{}, &jsonwebtoken.AdditionalInfo{}, nil)
+		mockSession.EXPECT().AddSession(gomock.Any(), gomock.Any()).Return(errors.New("session error"))
+
+		_, err := s.SignUp(ctx, user)
+		assert.Error(t, err)
+	})
+
+	t.Run("error when user already exists", func(t *testing.T) {
+		mockDatabase.EXPECT().InsertUser(gomock.Any(), gomock.Any()).Return(&uuid.UUID{}, errors.New("user already exists"))
+
+		_, err := s.SignUp(ctx, user)
+		assert.Error(t, err)
+	})
+}
